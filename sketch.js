f18cd237dbf4c7561e82c91ebf45ea238ce8ffd3
arkjoshi <hetalbjjoshi@gmail.com>
2021-02-06 14:46:21 +0530

    modified

--- sketch.js
+++ sketch.js
@@ -65,4 +65,13 @@ function draw(){
     platform.display();
     //log6.display();
     Slingshot.display();    
+    
+}
+function mouseDragged()
+    {
+        Matter.Body.setPosition(bird.body,{x:mouseX, y:mouseY} );
+    }
+function mouseReleased()
+{
+    Slingshot.fly();
 }
